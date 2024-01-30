5fd4e2889724574262db8863a54d53c553c6efcc
Chigbu Joshua <chigbujoshua@yahoo.com>
2024-01-30 13:39:39 +0100

    Is AVL

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,47 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
+/**
+ * binary_tree_is_avl - finds if a binary tree is an avl
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 1 if tree is avl
+ *         0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (avl_helper(tree, INT_MIN, INT_MAX));
+}
+
+/**
+ * avl_helper - helper that finds if a binary tree is an avl
+ * @tree: pointer to the root node of the tree
+ * @min: minimum value
+ * @max: maximum value
+ *
+ * Return: 1 if tree is avl
+ *         0 otherwise
+ */
+int avl_helper(const binary_tree_t *tree, int min, int max)
+{
+	int path_l, path_r;
+
+	if (!tree)
+		return (1);
+	if (tree->n < min || tree->n > max)
+		return (0);
+
+	path_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	path_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	if (abs(path_l - path_r) > 1)
+		return (0);
+
+	return (avl_helper(tree->left, min, tree->n - 1) &&
+		avl_helper(tree->right, tree->n + 1, max));
+	/* This is part of the BST check logic */
+}
+
--- binary_trees.h
+++ binary_trees.h
@@ -60,9 +60,12 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
-bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value);
-bst_t *bst_delete(bst_t *root, bst_t *node);
-bst_t *inorder_successor(bst_t *root);
+int successor(bst_t *node);
+int two_children(bst_t *root);
+int remove_type(bst_t *root);
+int binary_tree_is_avl(const binary_tree_t *tree);
+int avl_helper(const binary_tree_t *tree, int min, int max);
+
 
 
 

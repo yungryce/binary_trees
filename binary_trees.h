5fa0ea3a5ff1c8bd65002f53960fff2f1d3d38e6
Chigbu Joshua <chigbujoshua@yahoo.com>
2024-01-30 13:11:05 +0100

    BST - Insert

--- binary_trees.h
+++ binary_trees.h
@@ -53,7 +53,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 int bst_helper(const binary_tree_t *tree, int min, int max);
-
+bst_t *bst_insert(bst_t **tree, int value);
 
 
 

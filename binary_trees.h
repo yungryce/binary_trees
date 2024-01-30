9c8128d7fc86b84f486b0ea84a3fc6dfb2ae6cef
Chigbu Joshua <chigbujoshua@yahoo.com>
2024-01-30 17:55:56 +0100

    AVL - Remove

--- binary_trees.h
+++ binary_trees.h
@@ -68,5 +68,14 @@ int avl_helper(const binary_tree_t *tree, int min, int max);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval);
 avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+bst_t *bst_remove(bst_t *root, int value);
+int remove_type(bst_t *root);
+int successor(bst_t *node);
+void bal(avl_t **tree);
+
+
+
+
 
 #endif /* _BINARY_TREES_H_ */

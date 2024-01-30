4310f361476b6d7eb9c987797622457446fdc83d
Chigbu Joshua <chigbujoshua@yahoo.com>
2024-01-30 13:18:16 +0100

    BST - Array to BST

--- binary_trees.h
+++ binary_trees.h
@@ -57,7 +57,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 int bst_helper(const binary_tree_t *tree, int min, int max);
 bst_t *bst_insert(bst_t **tree, int value);
-
+bst_t *array_to_bst(int *array, size_t size);
 
 
 
